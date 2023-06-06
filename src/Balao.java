52dc8184de3fbf9dab59149568d38599e9eea577
rm86461 <mayara123santos97@gmail.com>
2023-06-06 08:30:20 -0300

    aprendemos sobre balonismo, foi legal

--- src/Balao.java
+++ src/Balao.java
@@ -1,3 +1,16 @@
 public class Balao {
+    // Atributos
+    int id;
+    String Registro;
+    String Cor;
+    String tipo;
+    int capacidade;
+    double altura;
+    String porte;
+    double peso;
+    // Metodos
+    public String voar(){
+        return "voando";
+    }
 
 }
